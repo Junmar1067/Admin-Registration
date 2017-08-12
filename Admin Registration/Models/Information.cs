253122ba04d1b65a0e364c2245d28057e531c133
unknown <pt807@CSLAB124.CSSERVER.local>
2017-08-12 19:33:25 +0800

    edit

--- Admin Registration/Models/Information.cs
+++ Admin Registration/Models/Information.cs
@@ -24,9 +24,7 @@ namespace Admin_Registration.Models
         private Information()
         {
             Accounts = new List<Account>();
-            Accounts.Add(new Account { Id = 1, Name = "My name 1", Age = 12 });
-            Accounts.Add(new Account { Id = 2, Name = "My name 2", Age = 12 });
-            Accounts.Add(new Account { Id = 3, Name = "My name 3", Age = 12 });
+            Accounts.Add(new Account { Id = 1, Username = "Admin1", Password = "12345", Name = "My name 1", Age = 12, Address = "Tayud" });
 
 
         }
